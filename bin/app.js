35bc250b0fb6fb424593500ac7e1c80032293c9d
--replace-all <felixtobin@hotmail.com>
2016-09-19 21:24:43 +0930

    changed port number

--- bin/app.js
+++ bin/app.js
@@ -15,6 +15,6 @@ app.get('/', function (req, res) {
   //res.render('index');
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("DON'T PANIC")
 });
